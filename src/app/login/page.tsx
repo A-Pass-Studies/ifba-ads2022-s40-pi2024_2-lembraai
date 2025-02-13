9b05c222bfa662689a609639c622579440cb6a21
Hozato <202211240020@ifba.edu.br>
2025-02-12 23:42:15 -0300

    Adição do login e do css
    Foi adcionado o login juntamente com o css

---
+++ src/app/login/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react"
+
+export default function Home() {
+    return (
+        <div>
+        <img
+          src="lembraai-logo.png"
+          alt="logo"
+          className="login-logo"
+          width={232}
+          height={142}
+        />
+  
+        <div className="login-form">
+          <input type="email" placeholder="E-mail" name="uname" required />
+  
+          <input type="password" placeholder="Senha" name="psw" required />
+  
+          <button type="submit" onClick={() => (window.location.href = "index.html")}>
+            Entrar
+          </button>
+          
+          <a href="registro.html">Não é usuário? Cadastre-se</a>
+        </div>
+      </div>
+    )
+}
